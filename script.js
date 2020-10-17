30ab024e172b352aa2b95dc3a09f3ef086c58bdd
Nidvar <djayshine87@hotmail.com>
2020-10-17 17:47:57 +1100

    timer class created. made connection to the button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Drawing Animations')
+class my_timer {
+    constructor(start_button, pause_button, duration_input){
+        this.start_button = start_button;
+        this.pause_button = pause_button;
+        this.duration_input = duration_input;
+        this.start_button.addEventListener('click', this.start)
+    }
+    start=()=>{
+        console.log('start button clicked - '+ Math.random())
+    }
+}
+
+
+new my_timer(document.getElementById('start_button'))
