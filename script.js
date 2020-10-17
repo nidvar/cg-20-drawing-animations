6e47bae6faad7750938cb1e50f0d705236f534d7
Nidvar <djayshine87@hotmail.com>
2020-10-17 18:28:23 +1100

    pause button function added

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ class my_timer {
         this.start_button = start_button;
         this.pause_button = pause_button;
         this.duration_input = duration_input;
-        this.start_button.addEventListener('click', this.start)
+        this.start_button.addEventListener('click', this.start);
+        this.pause_button.addEventListener('click', this.pause);
     }
     start=()=>{
-        console.log('start button clicked - '+ Math.random())
+        this.tick();
+        this.ticking_interval = setInterval(this.tick, 1000)
+        console.log(this.ticking_interval)
+    }
+    tick=()=>{
+        console.log('tick')
+    }
+    pause=()=>{
+        console.log('pause')
+        clearInterval(this.ticking_interval);
     }
 }
 
 
-new my_timer(document.getElementById('start_button'))
+new my_timer(document.getElementById('start_button'), document.getElementById('pause_button'))
