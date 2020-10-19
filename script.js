05b8f34345350f83c3964d3d29c80f8af2e1c0cb
Nidvar <djayshine87@hotmail.com>
2020-10-19 12:38:00 +1100

    getters and setters

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const duration_input_from_dom = document.getElementById('duration_input');
+const duration_input_from_dom = document.getElementById('user_input');
 const pause_button_from_dom = document.getElementById('pause_button');
 const start_button_from_dom = document.getElementById('start_button');
 
 class my_timer {
-    constructor(start_button, pause_button, duration_input){
+    constructor(start_button, pause_button, user_input){
         this.start_button = start_button;
         this.pause_button = pause_button;
-        this.duration_input = duration_input;
+        this.user_input = user_input;
 
         this.start_button.addEventListener('click', this.start);
         this.pause_button.addEventListener('click', this.pause);
+        this.user_input.addEventListener('input', this.set_remaining_time);
     }
     
     start=()=>{
@@ -17,16 +18,24 @@ class my_timer {
         this.ticking_interval = setInterval(this.tick, 1000)
         console.log(this.ticking_interval)
     }
+
     tick=()=>{
-        let x = parseFloat(duration_input_from_dom.value)
-        x = x - 1
-        duration_input_from_dom.value = x
-        console.log(x)
+        this.set_remaining_time = this.get_remaining_time - 1;
+        console.log(`remaining time: ${this.user_input.value}`)
     }
+
     pause=()=>{
         console.log('pause')
         clearInterval(this.ticking_interval);
     }
+
+    get get_remaining_time(){
+        return this.user_input.value
+    }
+
+    set set_remaining_time(new_value){
+        this.user_input.value = new_value
+    }
 }
 
-const z = new my_timer(start_button_from_dom, pause_button_from_dom, duration_input_from_dom)
+const z = new my_timer(start_button_from_dom, pause_button_from_dom, user_input)
